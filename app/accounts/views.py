0f7d910083ebdcf52aee896401d297d1bca93f67
HyungtaeMoon <blessmht@gmail.com>
2018-10-27 18:55:06 +0900

    [ADD] profile 페이지 구현, @login_required 데코레이터 사용

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required # 로그인 상태일 때만 /profile 페이지로 접근 가능하도록
+def profile(request):
+    return render(request, 'accounts/profile.html')
