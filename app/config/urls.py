77a0fa5f95d4565b298028d3bbb9d587f9dba45b
HyungtaeMoon <blessmht@gmail.com>
2018-10-27 19:18:23 +0900

    [ADD] lambda를 사용하여 shop 페이지  redirect(페이지 미구현)

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^$', lambda request: redirect('shop:index'), name='root')
 ]
