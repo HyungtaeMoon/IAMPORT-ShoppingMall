1f2b2be88f5182abee1164434051c8adf0348581
HyungtaeMoon <blessmht@gmail.com>
2018-10-29 21:43:14 +0900

    [ADD] 결제없는 주문 구현

--- app/shop/urls.py
+++ app/shop/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'shop'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<item_id>\d+)/order/new/$', views.order_new, name='order_new'),
 ]
--- app/shop/views.py
+++ app/shop/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import ListView
 from .models import Item
-
+from .forms import OrderForm
 
 class ItemListView(ListView):
     model = Item
@@ -22,3 +23,24 @@ class ItemListView(ListView):
 
 
 index = ItemListView.as_view()
+
+
+@login_required
+def order_new(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    initial = {'name': item.name, 'amount': item.amount}
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST, initial=initial)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            order.item = item
+            order.save()
+            return redirect('profile')
+    else:
+        form = OrderForm(initial=initial)
+
+    return render(request, 'shop/order_form.html', {
+        'form': form,
+    })
