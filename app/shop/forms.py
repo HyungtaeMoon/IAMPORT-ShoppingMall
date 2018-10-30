a2a6206032c0d8ea2ca0f58ec376bc8f72781843
HyungtaeMoon <blessmht@gmail.com>
2018-10-30 16:03:03 +0900

    [ADD] PayForm에서 아임포트 API 결제 연동하여 결제시스템 구현

--- app/shop/forms.py
+++ app/shop/forms.py
@@ -1,4 +1,10 @@
+import json
+
 from django import forms
+from django.template.loader import render_to_string
+from django.utils.encoding import smart_text
+from django.utils.safestring import mark_safe
+
 from .models import Order
 
 
@@ -10,3 +16,32 @@ class OrderForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'readonly': 'readonly'}),
             'amount': forms.TextInput(attrs={'readonly': 'readonly'}),
         }
+
+
+class PayForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('imp_uid',)
+
+    def as_iamport(self):
+        # Form의 Hidden 필드 위젯
+        hidden_fields = mark_safe(''.join(smart_text(field) for field in self.hidden_fields()))
+
+        # IMP.request_pay의 인자로 넘길 인자 목록
+        fields = {
+            'merchant_uid': str(self.instance.merchant_uid),    # uuid4
+            'name': self.instance.name,
+            'amount': self.instance.amount,
+        }
+
+        return hidden_fields + render_to_string('shop/_iamport.html', {
+            'json_fields': mark_safe(json.dumps(fields, ensure_ascii=False)),
+            'iamport_shop_id': 'iamport',   # 각자의 상점 아이디를 입력
+        })
+
+    def save(self):
+        order = super().save(commit=False)
+        order.status = 'paid'   # 아임포트 API를 통한 확인 후에 변경
+        order.save()
+
+        return order
